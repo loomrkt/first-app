f8fa25139f570b95740755c199f1777d5eb1f524
loomrkt <mrnrakotondrazaka@gmail.com>
2023-11-02 00:12:43 +0200

    theme apparence

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Navbar from "./components/navbar";
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import HSThemeAppearance from "./utils/HSThemeAppearance"; // Import HSThemeAppearance
 
 function App() {
+  useEffect(() => {
+    HSThemeAppearance();
+  }, []);
   return (
     <>
       <Navbar />
